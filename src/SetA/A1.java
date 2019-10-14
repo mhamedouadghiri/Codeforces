6de11c90f78089a7aae0620f25da6289d35e01fd
mhamedouadghiri <mhamed.sefianeelouadghiri@gmail.com>
2019-10-14 20:29:15 +0100

    initial commit

---
+++ src/SetA/A1.java
@@ -0,0 +1,18 @@
+package SetA;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class A1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int a = scanner.nextInt();
+        int x = (int) Math.ceil((double) n / a);
+        int y = (int) Math.ceil((double) m / a);
+        BigInteger xx = new BigInteger(String.valueOf(x));
+        BigInteger yy = new BigInteger(String.valueOf(y));
+        System.out.println(xx.multiply(yy));
+    }
+}
