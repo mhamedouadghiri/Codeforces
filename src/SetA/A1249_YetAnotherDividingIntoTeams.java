fc1ae66a5b3b5f0294a8d79f931c28806ee4d68a
mhamedouadghiri <mhamed.sefianeelouadghiri@gmail.com>
2019-11-16 17:40:25 +0100

    Initial Commit

---
+++ src/SetA/A1249_YetAnotherDividingIntoTeams.java
@@ -0,0 +1,55 @@
+package SetA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A1249_YetAnotherDividingIntoTeams {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out);
+        int q = nextInt();
+        int n, cnt;
+        int[] arr;
+        while (q-- != 0) {
+            n = nextInt();
+            cnt = 1;
+            arr = Arrays.stream(nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(arr);
+            for (int i = 0; i < n - 1; i++)
+                cnt += arr[i + 1] - arr[i] == 1 ? 1 : 0;
+            pw.println(cnt == 1 ? 1 : 2);
+        }
+        pw.close();
+    }
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    private static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    private static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    private static String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
