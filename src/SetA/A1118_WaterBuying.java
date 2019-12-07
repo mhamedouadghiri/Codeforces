40bdaa2ca9fa9b66e0bbdcc050a9b4f33d247b08
mhamedouadghiri <mhamed.sefianeelouadghiri@gmail.com>
2019-12-08 00:20:45 +0100

    Initial Commit

---
+++ src/SetA/A1118_WaterBuying.java
@@ -0,0 +1,57 @@
+package SetA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A1118_WaterBuying {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out);
+        int q = nextInt();
+        while (q-- != 0) {
+            long n = nextLong();
+            int a = nextInt();
+            int b = nextInt();
+            pw.println(b - 2 * a >= 0 ? a * n : a * n + (b - 2 * a) * (n / 2));
+        }
+        pw.close();
+    }
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    private static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    private static String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    private static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    private static long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    private static double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
